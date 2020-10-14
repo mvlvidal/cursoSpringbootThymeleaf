5fea3779dcd856c060703d21891d7b48764abce4
mvlvidal <marcusvlvidal@gmail.com>
2020-10-14 14:01:26 -0300

    Implementação inicial do cadastro e listagem de pessoas

---
+++ src/main/java/curso/springboot/controller/PessoaController.java
@@ -0,0 +1,48 @@
+package curso.springboot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import curso.springboot.model.Pessoa;
+import curso.springboot.repository.PessoaRepository;
+
+@Controller
+public class PessoaController {
+
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/cadastropessoa")
+	public String inicio() {
+		return "cadastro/cadastropessoa";
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, value = "/salvarpessoa")
+	public ModelAndView salvar(Pessoa pessoa) {
+		
+		if(pessoa != null) {
+			pessoaRepository.save(pessoa);
+		}
+
+		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
+		Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
+		modelAndView.addObject("pessoas",pessoasIt);
+		
+		return modelAndView;
+		
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/listarpessoas")
+	public ModelAndView pessoas() {
+		
+		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
+		Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
+		modelAndView.addObject("pessoas",pessoasIt);
+		
+		return modelAndView;
+		
+	}
+}
